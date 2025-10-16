ee58bd8861dc3a25da94b66047b3e48cf2cd20bd
Peter karanja <peterkaranja60@gmail.com>
2025-10-16 16:05:32 +0300

    Update TabButtons.tsx

--- src/components/TabButtons.tsx
+++ src/components/TabButtons.tsx
@@ -1,25 +1,25 @@
 import React from "react";
 
-interface TabButtonProps {
-  id: string;
+interface TabButtonProps<T extends string | number = string> {
+  id: T;
   label: string;
   isActive: boolean;
-  onClick: (id: any) => void;
+  onClick: (id: T) => void;
   icon?: React.ElementType;
   count?: number;
 }
 
-export default function TabButtons({
+export default function TabButtons<T extends string | number = string>({
   id,
   label,
   isActive,
   onClick,
   icon: Icon,
   count,
-}: TabButtonProps) {
+}: TabButtonProps<T>) {
   return (
     <button
-      key={id}
+      key={String(id)}
       onClick={() => onClick(id)}
       className={`flex items-center justify-center px-6 sm:px-8 py-3 sm:py-4 rounded-lg font-semibold transition-all text-base sm:text-lg
         ${
@@ -27,7 +27,6 @@ export default function TabButtons({
             ? "bg-[#D4AF37] text-white shadow-lg"
             : "bg-gray-50 text-[#0D090A] hover:bg-gray-100 shadow"
         }
-     
       `}
     >
       {Icon && <Icon className="w-5 h-5 sm:w-6 sm:h-6 mr-2 sm:mr-3" />}
